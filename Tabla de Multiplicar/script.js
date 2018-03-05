119f1221dbeed53320f578fac9a796a325330f3e
jdmartinbr <jdmartinbr@users.noreply.github.com>
2018-03-05 21:28:30 +0100

    Tabla de multiplicar

---
+++ Tabla de Multiplicar/script.js
@@ -0,0 +1,11 @@
+function multiply(){
+    let a = document.getElementsByClassName("number")[0].value;
+    if (isNaN(a) || a === "") {
+        return alert('Introduce un número válido')
+    }
+    //isNaN(a) ? alert('Introduce un número válido');
+    for (var i = 0; i < 11; i++) {
+        let result = a * i;
+        document.getElementById("result").innerHTML += a + ' * ' + i + ' = ' + result +  "<br>";
+    }
+}
